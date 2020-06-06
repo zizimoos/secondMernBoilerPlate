a56494b108a5f590fe338872483ee6a83909688f
henry <zizimoos@gmail.com>
2020-06-06 22:56:41 +0900

    error bycript

--- index.js
+++ index.js
@@ -22,11 +22,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/", (req, res) => res.send(`Hello world`));
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
+  console.log(user);
+
   user.save((error, data) => {
-    try {
-      return res.status(200).json({ success: true });
-    } catch (error) {
+    console.log("data", data);
+    if (error) {
       return res.json({ success: false, error });
     }
+    res.status(200).json({ success: true, userData: data });
+  });
+});
+app.post("/api/user/login", (req, res) => {
+  //find email & user
+  console.log(User);
+  User.findOne({ email: req.body.email }, (error, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
+    }
+    //compare password
+    user.comparePassword(req.body.password, (error, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSuccess: false, message: "wrong password" });
+      }
+    });
+    //generate token
+    user.generateToken((error, user) => {
+      if (error) {
+        return res.status(400).send(error);
+      }
+      res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+      });
+    });
   });
 });
--- models/user.js
+++ models/user.js
@@ -1,20 +1,72 @@
 const mogngoose = require("mongoose");
+const bycrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mogngoose.Schema({
   email: {
     type: String,
     minlength: 5,
   },
-  name: { type: String, maxlength: 50 },
-  lastName: { type: String, maxlength: 50 },
-  password: { type: String, minlength: 5 },
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  lastName: {
+    type: String,
+    maxlength: 50,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
   role: {
     type: Number,
     default: 0,
   },
-  token: { type: String },
-  tokenExp: { type: Number },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
 });
+
+userSchema.pre("save", function (next) {
+  let user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, (error, salt) => {
+      if (error) return next(error);
+      bycrypt.hash(user.password, salt, function (error, hash) {
+        if (error) return next(error);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = (plainPassword, cb) => {
+  bycrypt.compare(plainPassword, this.password, (error, isMatch) => {
+    if (error) {
+      return cb(error);
+    }
+    cb(null, isMatch);
+  });
+};
+userSchema.methods.generateToken = (cb) => {
+  let user = this;
+  let token = jwt.sign(user._id.toHexString(), "secret");
+  user.token = token;
+  user.save((error, user) => {
+    if (error) {
+      return cb(error);
+    }
+    cb(null, user);
+  });
+};
 
 const User = mogngoose.model("User", userSchema);
 module.exports = { User };
